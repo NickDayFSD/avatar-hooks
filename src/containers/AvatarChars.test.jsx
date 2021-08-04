4a19f8e344a8e023cbe159eb7e5cc38c86cf45aa
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-04 09:37:17 -0700

    test passing

--- src/containers/AvatarChars.test.jsx
+++ src/containers/AvatarChars.test.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { MemoryRouter } from 'react-router-dom';
 import AvatarChars from './AvatarChars';
 
-describe.skip('Character List Loader', () => {
+const server = setupServer(
+  rest.get(
+    'https://last-airbender-api.herokuapp.com/api/v1/characters',
+    (req, res, ctx) => {
+      return res(
+        ctx.json([
+          {
+            _id: '3af4',
+            name: 'Aang',
+            photoUrl: 'none',
+            affiliation: 'Air Nomads',
+          },
+        ])
+      );
+    }
+  )
+);
+
+describe('Character List Loader', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
   test('Loader > UL of Avatar characters', async () => {
-    render(<AvatarChars />);
+    render(
+      <MemoryRouter>
+        <AvatarChars />
+      </MemoryRouter>
+    );
 
     screen.getByAltText('Loading');
 
-    const ul = await screen.findByRole('listitem');
+    const ul = await screen.findByRole('list');
     expect(ul).not.toBeEmptyDOMElement();
   });
 });
